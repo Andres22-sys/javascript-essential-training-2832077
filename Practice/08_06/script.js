573814dffcc05deb0471ed7aa4a9b56b6a7bd6fe
Andres Dominguez <adominguezl800@gmail.com>
2022-11-27 12:37:10 -0800

    git commit -m "08_06 pass value to function"

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -2,6 +2,32 @@
  * Practice: Pass values between functions
  *
  * - Create two functions
- * - Main function creates article element with data from object
+ * - Main function creates article 
+ * element with data from object
  * - Helper function creates.
  */
+
+const blackJacket = {
+  name: "Black Jacket",
+  color: "black",
+  volume: 10,
+  pocketNum: 4,
+};
+
+const addPocket = (currentPocket) => {
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML = `
+      <h1>${currentPocket.name}</h1>
+      <ul>
+        <li>Volume: ${currentPocket.volume}</li>
+        <li>Color: ${currentPocket.color}</li>
+        <li>Number of pockets: ${currentPocket.pocketNum}</li>
+      </ul>
+  `;
+  return newArticle;
+};
+
+const main = document.querySelector("main");
+main.append(addPocket(blackJacket));
+console.log(addPocket(blackJacket));
+
